2b0b1c1d45227d0f81f30e3d4083c0e7ddbad234
Suyash Lakhotia <suyashlakhotia@gmail.com>
2017-03-03 20:48:42 +0800

    Changed Range of SR Sensor

--- src/robot/RobotConstants.java
+++ src/robot/RobotConstants.java
@@ -15,7 +15,7 @@ public class RobotConstants {
     public static final int TURN_COST = 20;                         // cost of RIGHT, LEFT movement
     public static final DIRECTION START_DIR = DIRECTION.NORTH;      // start direction
     public static final int SENSOR_SHORT_RANGE_L = 1;               // range of short range sensor (cells)
-    public static final int SENSOR_SHORT_RANGE_H = 3;               // range of short range sensor (cells)
+    public static final int SENSOR_SHORT_RANGE_H = 2;               // range of short range sensor (cells)
     public static final int SENSOR_LONG_RANGE_L = 1;                // range of long range sensor (cells)
     public static final int SENSOR_LONG_RANGE_H = 7;                // range of long range sensor (cells)
 
