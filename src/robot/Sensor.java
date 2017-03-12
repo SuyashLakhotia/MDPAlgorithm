0b18fce9d9e9dc09a8ca59451df2f9b1bce3e992
Suyash Lakhotia <suyashlakhotia@gmail.com>
2017-03-13 01:15:37 +0800

    Refactored Code

--- src/robot/Sensor.java
+++ src/robot/Sensor.java
@@ -31,28 +31,6 @@ public class Sensor {
     }
 
     /**
-     * Sets the appropriate obstacle cell in the map and returns the row or column value of the obstacle cell. Returns
-     * -1 if no obstacle is detected.
-     */
-    public int getSensorVal(Map exploredMap, Map realMap, int rowInc, int colInc) {
-        for (int i = this.lowerRange; i <= this.upperRange; i++) {
-            int row = this.sensorPosRow + (rowInc * i);
-            int col = this.sensorPosCol + (colInc * i);
-
-            if (!realMap.checkValidCoordinates(row, col)) {
-                return i;
-            }
-
-            exploredMap.getCell(row, col).setIsExplored(true);
-            if (realMap.getCell(row, col).getIsObstacle()) {
-                exploredMap.setObstacleCell(row, col, true);
-                return i;
-            }
-        }
-        return 0;
-    }
-
-    /**
      * Returns the number of cells to the nearest detected obstacle or -1 if no obstacle is detected.
      */
     public int sense(Map exploredMap, Map realMap) {
@@ -70,21 +48,42 @@ public class Sensor {
     }
 
     /**
+     * Sets the appropriate obstacle cell in the map and returns the row or column value of the obstacle cell. Returns
+     * -1 if no obstacle is detected.
+     */
+    public int getSensorVal(Map exploredMap, Map realMap, int rowInc, int colInc) {
+        for (int i = this.lowerRange; i <= this.upperRange; i++) {
+            int row = this.sensorPosRow + (rowInc * i);
+            int col = this.sensorPosCol + (colInc * i);
+
+            if (!exploredMap.checkValidCoordinates(row, col)) return i;
+
+            exploredMap.getCell(row, col).setIsExplored(true);
+
+            if (realMap.getCell(row, col).getIsObstacle()) {
+                exploredMap.setObstacleCell(row, col, true);
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
      * Uses the sensor direction and given value from the actual sensor to update the map.
      */
     public void senseReal(Map exploredMap, int sensorVal) {
         switch (sensorDir) {
             case NORTH:
-                processSensorVal(exploredMap, 1, 0, sensorVal);
+                processSensorVal(exploredMap, sensorVal, 1, 0);
                 break;
             case EAST:
-                processSensorVal(exploredMap, 0, 1, sensorVal);
+                processSensorVal(exploredMap, sensorVal, 0, 1);
                 break;
             case SOUTH:
-                processSensorVal(exploredMap, -1, 0, sensorVal);
+                processSensorVal(exploredMap, sensorVal, -1, 0);
                 break;
             case WEST:
-                processSensorVal(exploredMap, 0, -1, sensorVal);
+                processSensorVal(exploredMap, sensorVal, 0, -1);
                 break;
         }
     }
@@ -92,7 +91,7 @@ public class Sensor {
     /**
      * Sets the correct cells to explored and/or obstacle according to the actual sensor value.
      */
-    public void processSensorVal(Map exploredMap, int rowInc, int colInc, int sensorVal) {
+    public void processSensorVal(Map exploredMap, int sensorVal, int rowInc, int colInc) {
         for (int i = this.lowerRange; i <= this.upperRange; i++) {
             int row = this.sensorPosRow + (rowInc * i);
             int col = this.sensorPosCol + (colInc * i);
